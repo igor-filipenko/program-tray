e892064cd5e6ddc1f12991167d51e29d2c9a0b08
igor-filipenko <i.filipenko@crystals.ru>
2025-04-11 20:43:50 +0300

    tests

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
-use toml;
-use std::fs;
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs;
+use std::str::FromStr;
+use toml;
+use regex::Regex;
 
 #[derive(Debug, Deserialize)]
 pub struct Program {
@@ -9,6 +11,28 @@ pub struct Program {
     command: String,
     args: HashMap<String, String>,
     env: HashMap<String, String>,
+}
+
+impl Program {
+
+    pub fn title(&self) -> &String { &self.title }
+    pub fn args(&self) -> &HashMap<String, String> { &self.args }
+    pub fn env(&self) -> &HashMap<String, String> { &self.env }
+
+    pub fn command(&self) -> String {
+        // Create a regex to match placeholders like $arg
+        let re = Regex::new(r"\$(\w+)").unwrap();
+
+        // Replace each match with the corresponding value from the map
+        re.replace_all(&self.command, |caps: &regex::Captures| {
+            // Extract the argument name (e.g., "arg" from "$arg")
+            let arg_name = String::from_str(caps.get(1).unwrap().as_str()).unwrap();
+
+            // Look up the argument in the map and return its value or leave it unchanged if not found
+            self.args.get(&arg_name).cloned().unwrap_or_else(|| format!("${}", arg_name))
+        }).to_string()
+    }
+
 }
 
 pub fn parse_properties_file(file_path: &str) -> Result<Program, Box<dyn std::error::Error>> {
@@ -33,4 +57,41 @@ fn parse_content(content: &str) -> Result<Program, Box<dyn std::error::Error>> {
             Err(Box::new(error))
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn read_config() -> io::Result<()> {
+        // Create a temporary file
+        let temp_file = NamedTempFile::new()?;
+
+        // Write some data to the file
+        temp_file.as_file().write_all(br#"
+          title = "title1"
+          command = "command1 $arg1"
+
+          [args]
+          arg1 = "arg2"
+
+          [env]
+          ENVVAR = "env1"
+        "#)?;
+
+        let program = match parse_properties_file(temp_file.path().to_str().unwrap()) {
+            Ok(program) => program,
+            Err(_) => panic!("Error reading file"),
+        };
+        assert_eq!(program.title(), "title1");
+        assert_eq!(program.command(), "command1 arg2");
+        assert_eq!(program.args().get("arg1").unwrap(), "arg2");
+        assert_eq!(program.env().get("ENVVAR").unwrap(), "env1");
+        Ok(())
+    }
+
 }
---
+++ tests/main.rs
@@ -0,0 +1,15 @@
+use assert_cmd::Command;
+
+#[test]
+fn test_no_args() {
+    let mut cmd = Command::cargo_bin("program-tray").unwrap();
+    cmd.assert().failure().code(1)
+        .stderr("Usage: program-tray <config-toml-file-path>\n");
+}
+
+#[test]
+fn test_file_not_found() {
+    let mut cmd = Command::cargo_bin("program-tray").unwrap();
+    cmd.arg("not-exists-file").assert().failure()
+        .stderr("Failed to read not-exists-file with error: No such file or directory (os error 2)\n");
+}
